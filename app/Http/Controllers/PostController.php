40dfadc3c4b2caa8275924e72125837bc2873559
koheihida <88849254+koheihida@users.noreply.github.com>
2021-12-25 15:47:15 +0900

    Initial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+
+
+    public function index()
+    {
+        $posts = Post::latest()->get();
+
+        return view('index')
+            ->with(['posts' => $posts]);
+    }
+    public function show(Post $post)
+    {
+        // $post = Post::findOrFail($id);
+
+        return view('posts.show')
+            // ->with(['post' => $this->posts[$id]]);
+            ->with(['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request){
+
+        $request->validate([
+            'title' => 'required|min:3',
+            'body' => 'required',
+        ]);
+
+        $post = new Post();
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+
+        return redirect()
+            ->route('posts.index');
+    }
+}
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <x-slot name="title">
+        {{ $post->title }} - My BBS
+    </x-slot>
+
+    <div class="back-link">
+        &laquo; <a href="{{ route('posts.index') }}">Back</a>
+    <h1>{{ $post->title }}</h1>
+        <p>{{ $post->body }}</p>
+</x-layout>
+
