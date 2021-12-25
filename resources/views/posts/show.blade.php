bf0df302d84e651fafbffb72afff1016a44cd25b
koheihida <88849254+koheihida@users.noreply.github.com>
2021-12-25 16:52:37 +0900

    HTMLを文字列として投稿できるよにするための実装

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,6 +6,6 @@
     <div class="back-link">
         &laquo; <a href="{{ route('posts.index') }}">Back</a>
     <h1>{{ $post->title }}</h1>
-        <p>{!! nl2br($post->body) !!}</p>
+        <p>{!! nl2br(e($post->body)) !!}</p>
 </x-layout>
 
